a73de1641df0c2340bc1c196a028078f855102a6
Marcelo Reis <marcelo-fil@outlook.com>
2018-09-11 21:22:06 +0100

    Add !rules

---
+++ plugModules/commands/rules.js
@@ -0,0 +1,19 @@
+module.exports = function Command(bot) {
+  bot.plugCommands.register({
+    names: ["rules", "rule"],
+    minimumPermission: 0,
+    cooldownType: "perUse",
+    cooldownDuration: 60,
+    parameters: "[@username]",
+    description: "Links the community rules.",
+    async execute(rawData, { args }, lang) { // eslint-disable-line no-unused-vars
+      const username = args.join(" ").substr(1);
+      const users = bot.plug.getUsers();
+      const user = users.filter(u => u.username.toLowerCase() === username.toLowerCase())[0] ||
+				users.filter(u => u.username.toLowerCase().trim() === username.toLowerCase().trim())[0];
+
+      this.reply(lang.rules, { mention: user || "" }, 6e4);
+      return true;
+    },
+  });
+};
