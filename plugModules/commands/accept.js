1996e24ed55302125b73aee9a58b92e05c11b6f0
Marcelo Reis <marcelo-fil@outlook.com>
2018-09-11 21:17:29 +0100

    Give: Needs testing before continue

--- plugModules/commands/accept.js
+++ plugModules/commands/accept.js
@@ -9,7 +9,7 @@ module.exports = function Command(bot) {
     parameters: "",
     description: "Accept if someone gives you the position in the waitlist.",
     async execute(rawData, command, lang) { // eslint-disable-line no-unused-vars
-      const userHaveGives = await bot.redis.findGivePositionTo(rawData.raw.id);
+      const userHaveGives = await bot.redis.findGivePositionTo(rawData.raw.uid);
 
       if (!isObject(userHaveGives)) {
         return false;
--- plugModules/commands/give.js
+++ plugModules/commands/give.js
@@ -20,11 +20,10 @@ module.exports = function Command(bot) {
         this.reply(lang.userNotFound, {}, 6e4);
         return false;
       } else if (user.id === rawData.raw.uid) {
-        this.reply(lang.give.onSelf, {}, 6e4);
         return false;
       }
 
-      const userPosition = bot.plug.getWaitListPosition(rawData.raw.id);
+      const userPosition = bot.plug.getWaitListPosition(rawData.raw.uid);
       const toUserPosition = bot.plug.getWaitListPosition(user.id);
 
       if (userPosition < 1 && toUserPosition < 1) {
@@ -37,17 +36,16 @@ module.exports = function Command(bot) {
         return false;
       }
 
-      const userGiving = await bot.redis.findGivePosition(rawData.raw.id);
+      const userGiving = await bot.redis.findGivePosition(rawData.raw.uid);
       const userHaveGives = await bot.redis.findGivePositionTo(user.id);
 
       if (!isNil(userGiving) || !isNil(userHaveGives)) {
-        this.reply(lang.give.canGiveAgain, {}, 6e4);
+        this.reply(lang.give.cantGiveAgain, {}, 6e4);
         return false;
       }
 
-      await bot.redis.registerGivePosition(rawData.raw.id, user.id, userPosition);
+      await bot.redis.registerGivePosition(rawData.raw.uid, user.id, userPosition);
       this.reply(lang.give.isGiving, {
-        user: rawData.raw.un,
         toUser: user.username,
         position: userPosition,
       });
