fd655aa63d5bf09467ccfad6ccf7123ff2773e9b
Marcelo Reis <marcelo-fil@outlook.com>
2018-09-13 00:08:30 +0100

    Add shush

---
+++ plugModules/commands/shush.js
@@ -0,0 +1,19 @@
+module.exports = function Command(bot) {
+  bot.plugCommands.register({
+    names: ["shush"],
+    minimumPermission: 1000,
+    cooldownType: "perUse",
+    cooldownDuration: 60,
+    parameters: "[@username]",
+    description: "Calls out an user that asked for skips. Big no-no.",
+    async execute(rawData, { args }, lang) { // eslint-disable-line no-unused-vars
+      const username = args.join(" ").substr(1);
+      const users = bot.plug.getUsers();
+      const user = users.filter(u => u.username.toLowerCase() === username.toLowerCase())[0] ||
+				users.filter(u => u.username.toLowerCase().trim() === username.toLowerCase().trim())[0];
+
+      this.reply(lang.shush, { mention: user || "" }, 6e4);
+      return true;
+    },
+  });
+};
