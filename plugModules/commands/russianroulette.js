25ec6152a807ef580a2f946411c64a14e27744d1
Marcelo Reis <marcelo-fil@outlook.com>
2018-09-13 18:22:45 +0100

    Add Russian Roulette

---
+++ plugModules/commands/russianroulette.js
@@ -0,0 +1,125 @@
+const { isObject, isNaN } = require("lodash");
+const { ROOM_ROLE } = require("plugapi");
+
+module.exports = function Command(bot) {
+  bot.plugCommands.register({
+    names: ["russianroulette"],
+    minimumPermission: 2000,
+    cooldownType: "none",
+    cooldownDuration: 0,
+    parameters: "<check|end|start [duration] [price]>",
+    description: "Roulette management command, can check if a roulette can be ran, can end or start a roulette (with specified duration or price, both defaulting to 60 and 1 respectively).",
+    async execute(rawData, { args }, lang) {
+      if (!args.length) {
+        this.reply(lang.roulette.noParams, {}, 6e4);
+        return false;
+      }
+
+      const params = ["check", "start", "end", "reset"];
+      const param = `${args.shift()}`.toLowerCase();
+
+      if (!params.includes(param)) {
+        this.reply(lang.roulette.invalidParam, {}, 6e4);
+        return false;
+      }
+
+      switch (param) {
+        case "check": {
+          if (await bot.roulette.check()) {
+            this.reply(lang.roulette.started, {}, 6e4);
+            return true;
+          }
+
+          const cooldown = await bot.redis.getCommandOnCoolDown("plug", "roulette@start", "perUse");
+
+          if (cooldown == -2) {
+            this.reply(lang.roulette.outOfCooldown, {}, 6e4);
+            return true;
+          }
+
+          this.reply(lang.roulette.onCooldown, {
+            elapsed: Math.floor((3600 - cooldown) / 60),
+            remaining: Math.ceil(cooldown / 60),
+          });
+          return true;
+        }
+        case "reset": {
+          const user = bot.plug.getUser(rawData.raw.uid);
+
+          if (!isObject(user) || await bot.utils.getRole(user) <= ROOM_ROLE.MANAGER) return false;
+
+          await bot.redis.removeCommandFromCoolDown("plug", "roulette@start", "perUse");
+          this.reply(lang.roulette.reset, {}, 6e4);
+          return true;
+        }
+        case "start": {
+          if (await bot.roulette.check()) {
+            this.reply(lang.roulette.started, {}, 6e4);
+            return true;
+          }
+
+          const cooldown = await bot.redis.getCommandOnCoolDown("plug", "roulette@start", "perUse");
+
+          if (cooldown != -2) {
+            this.reply(lang.roulette.onCooldown, {
+              elapsed: Math.floor((3600 - cooldown) / 60),
+              remaining: Math.ceil(cooldown / 60),
+            });
+            return true;
+          }
+
+          let duration = 60;
+
+          if (args.length) {
+            const specifiedDuration = parseInt(args.shift(), 10);
+
+            if (isNaN(specifiedDuration) || specifiedDuration < 10 || specifiedDuration > 120) {
+              this.reply(lang.roulette.invalidDuration, {}, 6e4);
+              return false;
+            }
+
+            duration = specifiedDuration;
+          }
+
+          let price = 1;
+
+          if (args.length) {
+            const specifiedPrice = parseInt(args.shift(), 10);
+
+            //if (specifiedPrice === 0) {
+            //   specifiedPrice = 1;
+            //}
+
+            if (isNaN(specifiedPrice) && specifiedPrice <= 100) {
+              this.reply(lang.roulette.invalidPrice, {}, 6e4);
+              return false;
+            }
+
+            price = specifiedPrice;
+          }
+
+          await bot.roulette.start(duration, price);
+          this.reply(lang.roulette.starting, {}, duration * 1e3);
+
+          await bot.plug.sendChat(bot.utils.replace(lang.roulette.info, {
+            duration,
+            price: price === 0 ? lang.roulette.free : `${price} prop${price > 1 ? "s" : ""}`,
+          }), duration * 1e3);
+          return true;
+        }
+        case "end": {
+          if (!await bot.roulette.check()) {
+            this.reply(lang.roulette.notStarted, {}, 6e4);
+            return false;
+          }
+
+          bot.roulette.end();
+          this.reply(lang.roulette.stopped, {}, 6e4);
+          return true;
+        }
+        default:
+          return false;
+      }
+    },
+  });
+};
---
+++ plugModules/utils/russianroulette.js
@@ -0,0 +1,112 @@
+const { each, sample } = require("lodash");
+
+module.exports = function Util(bot) {
+  class RussianRouletteUtil {
+    constructor() {
+      this.running = false;
+      this.price = undefined;
+      this.duration = undefined;
+      this.players = [];
+    }
+    async start(duration, price) {
+      this.running = true;
+      this.duration = duration;
+      this.price = price;
+
+      await bot.redis.placeCommandOnCooldown("plug", "russianroulette@start", "perUse", 1, 3600);
+
+      this.timeout = setTimeout(async () => {
+        await this.chooseVictim(this.players);
+      }, duration * 1e3);
+    }
+    end() {
+      this.running = false;
+      this.price = undefined;
+      this.duration = undefined;
+      this.players = [];
+
+      clearTimeout(this.timeout);
+
+      return true;
+    }
+    async check(cooldown) {
+      if (cooldown) {
+        return bot.redis.getCommandOnCoolDown("plug", "russianroulette@start", "perUse");
+      }
+
+      return this.running;
+    }
+    add(id) {
+      if (!this.players.includes(id)) {
+        this.players.push(id);
+        return true;
+      }
+
+      return false;
+    }
+    async chooseVictim(players) {
+      this.running = false;
+
+      const victim = players[Math.floor(Math.random() * players.length)];
+      const user = bot.plug.getUser(victim);
+      const waitlist = bot.plug.getWaitList();
+
+      if (!players.length) {
+        await bot.plug.sendChat(bot.lang.russianroulette.countOver);
+        this.end();
+        return;
+      }
+
+      if (!user || typeof user.username !== "string" || !user.username.length) {
+        await bot.plug.sendChat(bot.utils.replace(bot.lang.russianroulette.chicken, {
+          user: victim,
+        }));
+
+        await bot.redis.removeDisconnection(victim);
+        this.chooseVictim(players.filter(player => player !== victim));
+        return;
+      }
+
+      await bot.plug.sendChat(bot.utils.replace(bot.lang.russianroulette.shot, {
+        user: user.username,
+      }));
+      await bot.wait(3000);
+
+      if (bot.plug.getWaitListPosition(victim) === -1) {
+        await bot.moderateMuteUser(user.id, bot.plug.MUTE._REASON.VIOLATING_COMMUNITY_RULES, bot.plug.MUTE.SHORT);
+        await bot.wait(1000);
+
+        this.chooseVictim(players.filter(player => player !== victim));
+        return;
+      }
+
+      const randomBool = Math.random() >= 0.5;
+
+      const luckyshot = Math.floor(Math.random() * (bot.plug.getWaitListPosition(victim) - 5)) + 4;
+      const unluckyshot = Math.floor(Math.random() * (waitlist.length - bot.plug.getWaitListPosition(victim))) + waitlist.length;
+
+      if (randomBool) {
+        await bot.plug.sendChat(bot.utils.replace(bot.lang.russianroulette.luckyshot, {
+          user: user.username,
+        }));
+        await bot.wait(2000);
+
+        bot.queue.add(user, luckyshot);
+        await bot.wait(1000);
+      }
+      else {
+        await bot.plug.sendChat(bot.utils.replace(bot.lang.russianroulette.unluckyshot, {
+          user: user.username,
+        }));
+        await bot.wait(2000);
+
+        bot.queue.add(user, unluckyshot);
+        await bot.wait(1000);
+      }
+
+      this.chooseVictim(players.filter(player => player !== victim));
+    }
+  }
+
+  bot.russianRoulette = new RussianRouletteUtil();
+};
