0c44e343446ad735d5ecc5448d2c48e8dee1ba5e
Marcelo Reis <marcelo-fil@outlook.com>
2018-09-23 21:06:41 +0100

    Punish other room promoters

--- plugModules/events/plug/chat.js
+++ plugModules/events/plug/chat.js
@@ -1,5 +1,6 @@
 const { each } = require("lodash");
 const moment = require("moment");
+const Discord = require("discord.js");
 
 module.exports = function Event(bot, platform) {
   const event = {
@@ -52,6 +53,28 @@ module.exports = function Event(bot, platform) {
             new bot.plugCommands.Class(bot, rawData, command);
           }
         });
+      }
+
+      const dubtrack = /^(?=.*join)(?=.*dubtrack.fm)/;
+      const plug = /^(.*join|.*room).*plug.dj./;
+
+      if (dubtrack.test(rawData.message) || plug.test(rawData.message)) {
+        bot.plug.moderateDeleteChat(rawData.id);
+        await bot.plug.moderateBanUser(rawData.from.id, bot.plug.BAN_REASON.NEGATAIVE_ATTITUDE, bot.plug.BAN.PERMA);
+
+        const embed = new Discord.RichEmbed()
+          .setAuthor(rawData.from.username, "http://icons.iconarchive.com/icons/paomedia/small-n-flat/64/sign-ban-icon.png")
+          .setColor(0xFF00FF)
+          .setFooter("By OtterBot")
+          .setTimestamp()
+          .addField("ID", rawData.from.id, true)
+          .addField("Type", "Ban", true)
+          .addField("Time", "Permanent", true)
+          .addField("Reason", "Promote other room", false)
+          .addField("Message", rawData.message, false);
+
+        bot.channels.get("485173444330258454").send({embed});
+        bot.channels.get("486637288923725824").send({embed});
       }
 
       if (/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(rawData.message)) {
