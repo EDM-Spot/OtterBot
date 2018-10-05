c3accb08efa1b569610c9d0ca58301be6e772dd6
Marcelo Reis <marcelo-fil@outlook.com>
2018-10-05 18:14:09 +0100

    Add reloading

---
+++ plugModules/commands/reload.js
@@ -0,0 +1,14 @@
+module.exports = function Command(bot) {
+  bot.commands.register({
+    names: ["reload"],
+    minimumPermission: 3000,
+    cooldownType: "perUse",
+    cooldownDuration: 10,
+    parameters: "",
+    description: "Reloads the bot.",
+    async execute() {
+      setTimeout(() => process.exit(1), 1e3);
+      return true;
+    },
+  });
+};
