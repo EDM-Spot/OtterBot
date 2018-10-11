e31f32992a82a765025267b299875f4c9ad13d95
Marcelo Reis <marcelo-fil@outlook.com>
2018-10-11 17:25:41 +0100

    Change

--- plugModules/events/plug/advance.js
+++ plugModules/events/plug/advance.js
@@ -93,19 +93,21 @@ module.exports = function Event(bot, filename, platform) {
 
       const songHistory = await bot.utils.getSongHistory(songAuthor, songTitle, data.media.cid);
 
-      if (!isNil(songHistory) || !bot.global.ignoreHistoryNext) {
-        if (songHistory.skip) {
-          if (!songHistory.maybe) {
-            await bot.plug.sendChat(bot.utils.replace(bot.lang.historySkip, {
-              time: bot.moment(map(songHistory, "created_at")[0]).fromNow(),
-            }));
-            //await bot.plug.sendChat("!plays");
-            await bot.plug.moderateForceSkip();
-          } else {
-            await bot.plug.sendChat(bot.utils.replace(bot.lang.maybeHistorySkip, {
-              cid: map(songHistory, "cid")[0],
-              time: bot.moment(map(songHistory, "created_at")[0]).fromNow(),
-            }));
+      if (!bot.global.ignoreHistoryNext) {
+        if (!isNil(songHistory)) {
+          if (songHistory.skip) {
+            if (!songHistory.maybe) {
+              await bot.plug.sendChat(bot.utils.replace(bot.lang.historySkip, {
+                time: bot.moment(map(songHistory, "created_at")[0]).fromNow(),
+              }));
+              //await bot.plug.sendChat("!plays");
+              await bot.plug.moderateForceSkip();
+            } else {
+              await bot.plug.sendChat(bot.utils.replace(bot.lang.maybeHistorySkip, {
+                cid: map(songHistory, "cid")[0],
+                time: bot.moment(map(songHistory, "created_at")[0]).fromNow(),
+              }));
+            }
           }
         }
       }
