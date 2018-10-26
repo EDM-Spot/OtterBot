36ca9e7ca74dd59c43f54ca36d7b5f404141ae97
Marcelo Reis <marcelo-fil@outlook.com>
2018-10-26 15:43:11 +0100

    Fix

--- plugModules/commands/updateRDJ.js
+++ plugModules/commands/updateRDJ.js
@@ -10,7 +10,7 @@ module.exports = function Command(bot) {
       const users = await bot.db.models.users.findAll();
 
       for (let i = 0; i < users.length; i++) {
-        await bot.wait(1000);
+        await bot.wait(3000);
         bot.utils.updateRDJ(users[i].id);
       }
 
