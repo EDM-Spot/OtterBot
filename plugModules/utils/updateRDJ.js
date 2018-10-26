9a28e32ed10290f8f8fa436e06a1ec1187300a40
Marcelo Reis <marcelo-fil@outlook.com>
2018-10-26 16:35:36 +0100

    Debug

--- plugModules/utils/updateRDJ.js
+++ plugModules/utils/updateRDJ.js
@@ -87,7 +87,13 @@ module.exports = function Util(bot) {
           const userPoints = points + tolerance;
 
           if (userPoints < 700) {
-            await bot.plug.moderateSetRole(user.id, ROOM_ROLE.NONE);
+            await bot.plug.moderateSetRole(user.id, ROOM_ROLE.NONE, (error, data) => {
+              if (error) {
+                console.log(error);
+              } else {
+                console.log(data);
+              }
+            });
 
             if (!isNil(userDB.get("discord"))) {
               bot.guilds.get("485173051432894489").members.get(userDB.get("discord")).removeRole(role).catch(console.error);
@@ -101,7 +107,13 @@ module.exports = function Util(bot) {
           const joined = moment().diff(userDB.get("created_at"), "months");
 
           if (points >= 700 && joined >= 1 && playscount >= 150) {
-            await bot.plug.moderateSetRole(user.id, ROOM_ROLE.RESIDENTDJ);
+            await bot.plug.moderateSetRole(user.id, ROOM_ROLE.RESIDENTDJ, (error, data) => {
+              if (error) {
+                console.log(error);
+              } else {
+                console.log(data);
+              }
+            });
 
             if (!isNil(userDB.get("discord"))) {
               bot.guilds.get("485173051432894489").members.get(userDB.get("discord")).addRole(role).catch(console.error);
@@ -124,7 +136,13 @@ module.exports = function Util(bot) {
             const userPoints = points + tolerance;
 
             if (userPoints < 700) {
-              await bot.plug.moderateSetRole(offUser[0].id, ROOM_ROLE.NONE);
+              await bot.plug.moderateSetRole(offUser[0].id, ROOM_ROLE.NONE, (error, data) => {
+                if (error) {
+                  console.log(error);
+                } else {
+                  console.log(data);
+                }
+              });
   
               if (!isNil(userDB.get("discord"))) {
                 bot.guilds.get("485173051432894489").members.get(userDB.get("discord")).removeRoles(role).catch(console.error);
@@ -138,7 +156,13 @@ module.exports = function Util(bot) {
             const joined = moment().diff(userDB.get("created_at"), "months");
   
             if (points >= 700 && joined >= 1 && playscount >= 150) {
-              await bot.plug.moderateSetRole(offUser[0].id, ROOM_ROLE.RESIDENTDJ);
+              await bot.plug.moderateSetRole(offUser[0].id, ROOM_ROLE.RESIDENTDJ, (error, data) => {
+                if (error) {
+                  console.log(error);
+                } else {
+                  console.log(data);
+                }
+              });
   
               if (!isNil(userDB.get("discord"))) {
                 bot.guilds.get("485173051432894489").members.get(userDB.get("discord")).addRoles(role).catch(console.error);
