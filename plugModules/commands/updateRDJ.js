d8c529aa550272432391e381c189f2d149033edc
Marcelo Reis <marcelo-fil@outlook.com>
2018-10-26 20:28:40 +0100

    PromiseQueue

--- plugModules/commands/updateRDJ.js
+++ plugModules/commands/updateRDJ.js
@@ -1,3 +1,5 @@
+const PromiseQueue = require ("easy-promise-queue");
+
 module.exports = function Command(bot) {
   bot.plugCommands.register({
     names: ["updaterdj"],
@@ -7,10 +9,14 @@ module.exports = function Command(bot) {
     parameters: "",
     description: "Update RDJ.",
     async execute(rawData, command, lang) { // eslint-disable-line no-unused-vars
+      const pq = new PromiseQueue({concurrency: 1});
+
       const users = await bot.db.models.users.findAll();
 
       for (const user of users) {
-        await bot.utils.updateRDJ(user.id);
+        pq.add(() => {
+          bot.utils.updateRDJ(user.id);
+        });
       }
 
       console.log("Finished");
