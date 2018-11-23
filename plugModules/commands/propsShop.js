6f52b3decea11fd7f1e312b63afe6472a000b5ef
Marcelo Reis <marcelo-fil@outlook.com>
2018-11-23 16:14:11 +0000

    Start testing the badge system

---
+++ plugModules/commands/propsShop.js
@@ -0,0 +1,40 @@
+const { isNil } = require("lodash");
+
+module.exports = function Command(bot) {
+  bot.plugCommands.register({
+    names: ["buy", "shop"],
+    minimumPermission: 5000,
+    cooldownType: "perUser",
+    cooldownDuration: 1,
+    deleteInstantly: true,
+    parameters: "[badge <Image>]",
+    description: "Props Shop",
+    async execute(rawData, { args }, lang) {
+      const { id } = rawData.from;
+
+      const buyType = rawData.args.slice(1).shift();
+      const url = rawData.args.slice(2).join(" ");
+
+      if (isNil(buyType) || isNil(url)) {
+        return false;
+      }
+
+      if (args.length && buyType === "badge") {
+        const [inst] = await bot.db.models.users.findOrCreate({ where: { id }, defaults: { id } });
+
+        const props = inst.get("props");
+
+        if (props < 100) {
+          this.reply(lang.join.noProps, {}, 6e4);
+          return true;
+        }
+
+        await bot.shop.saveImage(id, url);
+
+        return true;
+      }
+
+      return false;
+    },
+  });
+};
---
+++ plugModules/utils/propsShop.js
@@ -0,0 +1,60 @@
+const { isNil } = require("lodash");
+const { ROOM_ROLE } = require("plugapi");
+const probe = require("probe-image-size");
+const download = require("image-downloader");
+
+module.exports = function Util(bot) {
+  class API {
+    constructor() {
+
+    }
+    async saveImage(id, url) {
+      const user = bot.plug.getUser(id);
+
+      if (isNil(url)) {
+        return;
+      }
+
+      probe(url).then(async result => {
+        const type = result.type;
+        const width = result.width;
+        const height = result.height;
+
+        if (width != 65 ||height != 65) {
+          return await bot.plug.sendChat("Badge should be 65x65");
+        }
+
+        if (type != "jpg" && type != "jpeg" && type != "png" && type != "gif") {
+          return await bot.plug.sendChat("Image not recognized");
+        }
+
+        if (type === "gif" && user.role < ROOM_ROLE.RESIDENTDJ) {
+          return await bot.plug.sendChat("Only RDJ+ can have gifs");
+        }
+
+        const options = {
+          url: url,
+          dest: `./dashboard/public/images/badges/${id}.${type}`
+        };
+
+        download.image(options).then(async ({ filename, image }) => {
+          const [inst] = await bot.db.models.users.findOrCreate({ where: { id }, defaults: { id } });
+          console.log(filename);
+
+          //await inst.decrement("props", { by: 100 });
+
+          //await bot.db.models.users.update(
+            //{ badge: filename },
+            //{ where: { id: id }, defaults: { id: id }}
+          //);
+        }).catch((err) => {
+          console.warn(err);
+        });
+      });
+
+      return true;
+    }
+  }
+
+  bot.shop = new API();
+};
