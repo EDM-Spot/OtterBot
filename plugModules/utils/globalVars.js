3b4c12bed0dc8afaf350b2d75dabd1358591e1f2
Marcelo Reis <marcelo-fil@outlook.com>
2018-11-26 09:33:11 +0000

    Update Values

--- plugModules/utils/globalVars.js
+++ plugModules/utils/globalVars.js
@@ -5,15 +5,15 @@ module.exports = function Util(bot) {
       this.isSkippedByTimeGuard = false;
 
       this.pointsWeight = {
-        woots: 1.4,
-        mehs: 3.95,
-        grabs: 2.1,
-        propsGiven: 0.08,
-        messages: 0.033,
+        woots: 1.42,
+        mehs: 3.988,
+        grabs: 2.2,
+        propsGiven: 0.04,
+        messages: 0.024,
         ban: 1.35,
         wlban: 0.47,
-        mute: 0.22,
-        daysOffline: 0.2
+        mute: 0.28,
+        daysOffline: 0.16
       };
     }
   }
