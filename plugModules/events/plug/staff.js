5cf5b6e71ce8531eb1fdd12e5d7784ec69f4f966
Marcelo Reis <marcelo-fil@outlook.com>
2018-11-30 17:19:32 +0000

    Test

--- plugModules/events/plug/staff.js
+++ plugModules/events/plug/staff.js
@@ -29,11 +29,9 @@ module.exports = function Event(bot, filename, platform) {
 
       bot.channels.get("486637288923725824").send({embed});
 
-      console.log(data.users.map((item) => { if (item.user != null) { return `${item.user.id}`; }}));
-      console.log(data);
-      console.log(data.users.map((item) => { if (item.user != null) { return `${item.id}`; }}));
+      console.log(data.users.map((item) => { if (item.user != null) { return `${item.user.id}`; }})[0]);
 
-      await bot.utils.updateRDJ(data.users.map((item) => { if (item.user != null) { return `${item.user.id}`; }}));
+      await bot.utils.updateRDJ(data.users.map((item) => { if (item.user != null) { return `${item.user.id}`; }})[0]);
     },
     init() {
       bot.plug.on(this.name, this.run);
