5c08c1224a5342d4ddbe9d918a7b6e5698eaa82f
Marcelo Reis <marcelo-fil@outlook.com>
2019-03-14 11:55:58 +0000

    Add pp command

---
+++ plugModules/commands/pp.js
@@ -0,0 +1,16 @@
+module.exports = function Command(bot) {
+  bot.plugCommands.register({
+    names: ["pp"],
+    minimumPermission: 1000,
+    cooldownType: "perUse",
+    cooldownDuration: 21600,
+    parameters: "",
+    description: "Check Bot PP",
+    async execute(rawData) { // eslint-disable-line no-unused-vars
+      const me = bot.plug.getSelf();
+
+      await bot.plug.sendChat(`I have ${me.pp} PP. Level ${me.level}`);
+      return true;
+    },
+  });
+};
