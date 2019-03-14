22b6267aeaae94fcbe7febc878b8f6773c0386c1
Marcelo Reis <marcelo-fil@outlook.com>
2019-03-14 16:22:47 +0000

    WTF

--- util/poker.js
+++ util/poker.js
@@ -1,14 +1,9 @@
-// Original Version https://github.com/1Computer1/kaado/blob/master/src/games/PokerGame.js
-const Discord = require("discord.js");
-const Deck = require("./poker/deck.js");
-const { Hand } = require("pokersolver");
-
 module.exports = (client) => {
-  class PokerUtil {
+  class aswUtil {
     constructor() {
       this.maxPlayers = 8;
     }
   }
 
-  client.pokerUtil = new PokerUtil();
+  client.awd = new aswUtil();
 };
