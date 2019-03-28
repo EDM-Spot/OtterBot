cbd3e428431b9de60bb8bdaf6c65e2d3e35b1f05
Marcelo Reis <marcelo-fil@outlook.com>
2019-03-28 21:12:34 +0000

    Update sequelize

--- plugModules/models/props.js
+++ plugModules/models/props.js
@@ -17,6 +17,7 @@ module.exports = async function Model(bot, sequelize) {
     historyID: {
       type: sequelize.STRING,
       allowNull: false,
+      field: "historyID",
     },
     identifier: {
       type: sequelize.STRING,
