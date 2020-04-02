bf9cf5cb203f52c41d0531931ce826ec54897f5d
Marcelo Reis <marcelo-fil@outlook.com>
2020-04-02 22:40:17 +0100

    Fix

--- util/voice.js
+++ util/voice.js
@@ -42,15 +42,18 @@ module.exports = (client) => {
 
     console.log(newMember);
     if (newMember !== undefined) {
-      if (newMember.id === "486087139088400384") { return; }
-      if (newMember.channelID != "485173051432894493") { return; }
+      if (newMember.channelID !== null) {
+        if (newMember.id === "486087139088400384") { return; }
+        if (newMember.channelID != "485173051432894493") { return; }
 
-      if (voiceChannel.members.size >= 1) {
-        await client.voiceUtil.play();
+        if (voiceChannel.members.size >= 1) {
+          await client.voiceUtil.play();
+        }
       }
     }
 
     console.log(voiceChannel.members.some(user => user.id === '486087139088400384'));
+    console.log(voiceChannel.members.size);
     if (voiceChannel.members.some(user => user.id === '486087139088400384')) {
       if (voiceChannel.members.size === 1) {
         voiceChannel.leave();
