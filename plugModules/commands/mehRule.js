149995e8fba3c0ce2f64cacfacc0160172f8aed1
Marcelo Reis <marcelo-fil@outlook.com>
2020-04-04 16:49:31 +0100

    Fix

--- plugModules/commands/mehRule.js
+++ plugModules/commands/mehRule.js
@@ -9,7 +9,7 @@ module.exports = function Command(bot) {
     async execute(rawData, command, lang) { // eslint-disable-line no-unused-vars
       const usersCount = bot.plug.users().length;
 
-      const mehRule = Math.round((usersCount/100)*6);
+      const mehRule = Math.round((usersCount/100)*7);
       const hsMehRule = Math.round((usersCount/100)*4);
 
       this.reply(lang.mehRule, {
