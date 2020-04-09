1cd3279fea4746f7cb7a5b6eb4712d8e901ff4a2
Marcelo Reis <marcelo-fil@outlook.com>
2020-04-09 16:38:02 +0100

    fix

--- commands/uno.js
+++ commands/uno.js
@@ -130,18 +130,18 @@ class Uno extends Command {
 
           let drawn = false;
 
-          // if (args.length > 2) {
-          //   let cardsCheck = args;
-          //   while (cardsCheck.length) {
-          //     let card = await this.client.unoUtil.player.getCard(cardsCheck.splice(0, 2));
-          //     if (card === null) return;
-          //     if (!card) return message.reply("It doesn't seem like you have that card! Try again.");
+          if (args.length > 2) {
+            let cardsCheck = args;
+            while (cardsCheck.length) {
+              let card = await this.client.unoUtil.player.getCard(cardsCheck.splice(0, 2));
+              if (card === null) return;
+              if (!card) return message.reply("It doesn't seem like you have one of that cards! Try again.");
 
-          //     if (this.client.unoUtil.flipped.color || !card.wild || card.id !== this.client.unoUtil.flipped.id || card.color !== this.client.unoUtil.flipped.color) {
-          //       return message.reply("Sorry, you can't play one of your cards here!");
-          //     }
-          //   }
-          // }
+              if (card.id === "REVERSE" || card.id === "SKIP" || card.id === "+2" || card.id === "WILD" || card.id === "WILD+4") {
+                return message.reply("Sorry, you can't multiple play special cards!");
+              }
+            }
+          }
 
           while (args.length) {
             let card = await this.client.unoUtil.player.getCard(args.splice(0, 2));
