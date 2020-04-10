426ff920f088ac5a4972200e86df0a584c99d12d
Marcelo Reis <marcelo-fil@outlook.com>
2020-04-10 16:20:56 +0100

    fix

--- util/russianroulette.js
+++ util/russianroulette.js
@@ -40,7 +40,7 @@ module.exports = (client) => {
 
     addPlayer(id, bet) {
       if (!this.players.includes(id)) {
-        this.bets.push({id: id, bet: bet});
+        this.bets.push({ id: id, bet: bet });
         this.players.push(id);
         this.startingPlayers.push(id);
         return true;
@@ -53,22 +53,22 @@ module.exports = (client) => {
       const victim = players[Math.floor(Math.random() * players.length)];
       const user = this.guild.member(victim);
 
-      const userDB = await client.db.models.users.findOne({
-        where: {
-          discord: victim,
-        },
-      });
-
       if (!players.length) {
         client.channels.cache.get(this.channel).send('The Russian Roulette Ended!');
         await this.end();
         return;
       }
 
+      const userDB = await client.db.models.users.findOne({
+        where: {
+          discord: victim,
+        },
+      });
+
       await client.wait(3000);
 
       client.channels.cache.get(this.channel).send(`${user} pulled the trigger...`);
-      
+
       await client.wait(5000);
 
       const randomBool = Math.random() >= 0.5;
@@ -76,11 +76,11 @@ module.exports = (client) => {
       if (randomBool) {
         const playerBet = this.bets.find(element => element.id === victim);
 
-        client.channels.cache.get(this.channel).send(`${user} survived! Won ${playerBet.bet*2} Props. :feelsgoodman:`);
+        client.channels.cache.get(this.channel).send(`${user} survived! Won ${playerBet.bet * 2} Props. :FeelsGoodMan:`);
 
-        await userDB.increment("props", { by: playerBet.bet*2 });
+        await userDB.increment("props", { by: playerBet.bet * 2 });
       } else {
-        client.channels.cache.get(this.channel).send(`:otterarage::bulletgun::tfw::quinsplat: BANG! ${user}`);
+        client.channels.cache.get(this.channel).send(`:otterrage::bulletgun::FeelsBadMan::quinsplat: BANG! ${user}`);
       }
 
       this.chooseVictim(players.filter(player => player !== victim));
