0b4de0d603739d7ba582e3d322f3aa293022aac5
Marcelo Reis <marcelo-fil@outlook.com>
2020-04-10 18:48:42 +0100

    fix

--- util/uno.js
+++ util/uno.js
@@ -10,7 +10,7 @@ module.exports = (client) => {
       this.guild = client.guilds.cache.get("485173051432894489");
       this.channel = "485927387079639051";
 
-      this.minPlayers = 1;
+      this.minPlayers = 2;
       this.maxPlayers = 8;
 
       this.running = false;
