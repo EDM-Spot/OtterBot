efab9342b2e02df92b521b232de157e2c0e2b43b
Marcelo Reis <marcelo-fil@outlook.com>
2020-11-27 19:38:58 +0000

    fix

--- commands/updateroles.js
+++ commands/updateroles.js
@@ -15,39 +15,39 @@ class UpdateRoles extends Command {
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
     try {
-      const list = this.client.guilds.cache.get("485173051432894489");
+      const members = this.client.guilds.cache.get("485173051432894489").members.cache;
 
-      list.members.cache.forEach(async function(member, client) {
-        const userDB = await client.db.models.users.findOne({
+      for (var i = 0, len = members.length; i < len; i++) {
+        const userDB = await this.client.db.models.users.findOne({
           where: {
-            discord: member.user.id,
+            discord: members[i].user.id,
           },
         });
 
         if (!isNil(userDB)) {
           const statusRole = "695994210603630633";
-          await member.roles.add(statusRole).catch(console.error);
+          await members[i].roles.add(statusRole).catch(console.error);
   
-          console.log(member.user.username + " Account is linked with plug.dj!");
+          console.log(members[i].user.username + " Account is linked with plug.dj!");
 
           const rdjRole = "485174834448564224";
-          const plugUser = await client.plug.getUser(userDB.id);
+          const plugUser = await this.client.plug.getUser(userDB.id);
 
-          if (member.roles.has(rdjRole)) {
+          if (members[i].roles.has(rdjRole)) {
             if (plugUser.role != ROLE.DJ) {
-              await member.roles.remove(rdjRole).catch(console.error);
+              await members[i].roles.remove(rdjRole).catch(console.error);
 
-              console.log(member.user.username + " RDJ Role Removed!");
+              console.log(members[i].user.username + " RDJ Role Removed!");
             }
           } else { 
             if (plugUser.role === ROLE.DJ) {
-              await member.roles.add(rdjRole).catch(console.error);
+              await members[i].roles.add(rdjRole).catch(console.error);
 
-              console.log(member.user.username + " RDJ Role Added!");
+              console.log(members[i].user.username + " RDJ Role Added!");
             }
           }
         }
-      });
+      }
     } catch (e) {
       console.log(e);
     }
