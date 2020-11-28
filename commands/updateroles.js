8c146b24232b90315181e6e104c282268752a4a6
Marcelo Reis <marcelo-fil@outlook.com>
2020-11-28 15:36:04 +0000

    fix

--- commands/updateroles.js
+++ commands/updateroles.js
@@ -14,44 +14,47 @@ class UpdateRoles extends Command {
   }
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
-    message.guild.members.fetch().then(async member => {
-      try {
-        console.log("Checking " + member.user.username);
+    message.guild.members.fetch().then(async members => {
+      members.forEach(async member => {
+        try {
+          console.log("Checking " + member.user.username);
 
-        const userDB = await this.client.db.models.users.findOne({
-          where: {
-            discord: member.user.id,
-          },
-        });
+          const userDB = await this.client.db.models.users.findOne({
+            where: {
+              discord: member.user.id,
+            },
+          });
 
-        if (!isNil(userDB)) {
-          const statusRole = "695994210603630633";
-          await member.roles.add(statusRole).catch(console.error);
+          if (!isNil(userDB)) {
+            const statusRole = "695994210603630633";
+            await member.roles.add(statusRole).catch(console.warn);
 
-          console.log(member.user.username + " Account is linked with plug.dj!");
+            console.log(member.user.username + " Account is linked with plug.dj!");
 
-          const rdjRole = "485174834448564224";
-          const plugUser = await this.client.plug.getUser(userDB.id);
+            const rdjRole = "485174834448564224";
+            const plugUser = await this.client.plug.getUser(userDB.id);
 
-          if (member.roles.has(rdjRole)) {
-            if (plugUser.role != ROLE.DJ) {
-              await member.roles.remove(rdjRole).catch(console.error);
+            if (member.roles.has(rdjRole)) {
+              if (plugUser.role != ROLE.DJ) {
+                await member.roles.remove(rdjRole).catch(console.warn);
 
-              console.log(member.user.username + " RDJ Role Removed!");
-            }
-          } else {
-            if (plugUser.role === ROLE.DJ) {
-              await member.roles.add(rdjRole).catch(console.error);
+                console.log(member.user.username + " RDJ Role Removed!");
+              }
+            } else {
+              if (plugUser.role === ROLE.DJ) {
+                await member.roles.add(rdjRole).catch(console.warn);
 
-              console.log(member.user.username + " RDJ Role Added!");
+                console.log(member.user.username + " RDJ Role Added!");
+              }
             }
           }
         }
-      }
-      catch {
-        console.error("Failed to load");
-      }
-    }).catch(console.error);
+        catch {
+          console.warn("Failed to load");
+          console.warn(member);
+        }
+      });
+    }).catch(console.warn);
   }
 }
 
