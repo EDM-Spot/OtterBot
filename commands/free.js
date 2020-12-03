e1cd1598a6e91a161a85a959d10729c60f3ae095
Marcelo Reis <marcelo-fil@outlook.com>
2020-12-03 21:26:35 +0000

    Fix

--- commands/free.js
+++ commands/free.js
@@ -1,4 +1,5 @@
 const Command = require("../base/Command.js");
+const { isNil } = require("lodash");
 const moment = require("moment");
 
 class Free extends Command {
@@ -13,10 +14,6 @@ class Free extends Command {
   async run(message, args, level) { // eslint-disable-line no-unused-vars
     try {
       //message.delete();
-
-      if (message.channel.id !== "695987344280649839") {
-        return true;
-      }
 
       const cooldown = await this.client.redis.getCommandOnCoolDown("discord", "free@use", "perUser", message.author.id);
 
@@ -36,17 +33,21 @@ class Free extends Command {
         },
       });
 
-      if (isDecember) {
-        props = 10;
+      if (!isNil(userDB)) {
+        if (isDecember) {
+          props = 10;
 
-        returnMessage = "<:christmasbells:784162299992735795> Merry Christmas! You got your daily 10 props. Come back tomorrow for more free props! <:christmasbells:784162299992735795>";
+          returnMessage = "<:christmasbells:784162299992735795> Merry Christmas! You got your daily 10 props. Come back tomorrow for more free props! <:christmasbells:784162299992735795>";
+        }
+
+        await this.client.db.models.users.increment("props", { by: props, where: { id: userDB.get("id") } });
+
+        await this.client.redis.placeCommandOnCooldown("discord", "free@use", "perUser", message.author.id, 86400);
+
+        return await message.reply(returnMessage);
+      } else {
+        return await message.reply("Your Account isn't linked! Use -link <Plug ID>");
       }
-
-      await this.client.db.models.users.increment("props", { by: props, where: { id: userDB.get("id") } });
-
-      await this.client.redis.placeCommandOnCooldown("discord", "free@use", "perUser", message.author.id, 86400);
-
-      return await message.reply(returnMessage);
     } catch (e) {
       console.log(e);
     }
