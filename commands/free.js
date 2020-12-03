633097a7403fa60cc6faba19daa9a72af51fc0c3
Marcelo Reis <marcelo-fil@outlook.com>
2020-12-03 21:14:33 +0000

    Update

---
+++ commands/free.js
@@ -0,0 +1,56 @@
+const Command = require("../base/Command.js");
+const moment = require("moment");
+
+class Free extends Command {
+  constructor(client) {
+    super(client, {
+      name: "free",
+      description: "Daily Props.",
+      usage: "free"
+    });
+  }
+
+  async run(message, args, level) { // eslint-disable-line no-unused-vars
+    try {
+      //message.delete();
+
+      if (message.channel.id !== "695987344280649839") {
+        return true;
+      }
+
+      const cooldown = await this.client.redis.getCommandOnCoolDown("discord", "free@use", "perUser", message.author.id);
+
+      if (cooldown != -2) {
+        message.delete();
+        return true;
+      }
+
+      const isDecember = (moment().month() === 11);
+
+      let props = 2;
+      let message = "You got your daily 2 props. Come back tomorrow for more free props!";
+
+      const userDB = await this.client.db.models.users.findOne({
+        where: {
+          discord: message.author.id,
+        },
+      });
+
+      if (isDecember) {
+        props = 10;
+
+        message = "<:christmasbells:784162299992735795> Merry Christmas! You got your daily 10 props. Come back tomorrow for more free props! <:christmasbells:784162299992735795>";
+      }
+
+      await this.client.db.models.users.increment("props", { by: props, where: { id: userDB.get("id") } });
+
+      await this.client.redis.placeCommandOnCooldown("discord", "free@use", "perUser", message.author.id, 86400);
+
+      return await message.reply(message);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+module.exports = Free;
--- plugModules/commands/free.js
+++ plugModules/commands/free.js
@@ -1,3 +1,5 @@
+const moment = require("moment");
+
 module.exports = function Command(bot) {
   bot.plugCommands.register({
     names: ["free"],
@@ -7,9 +9,19 @@ module.exports = function Command(bot) {
     parameters: "",
     description: "Daily Props.",
     async execute(rawData, command, lang) { // eslint-disable-line no-unused-vars
-      await bot.db.models.users.increment("props", { by: 2, where: { id: rawData.uid } });
+      const isDecember = (moment().month() === 11);
 
-      bot.plug.chat("You got your daily 2 props. Come back tomorrow for more free props!");
+      let props = 2;
+
+      if (isDecember) {
+        props = 10;
+
+        bot.plug.chat(":christmasballs1: Merry Christmas! You got your daily 10 props. Come back tomorrow for more free props! :christmasballs1:");
+      } else {
+        bot.plug.chat("You got your daily 2 props. Come back tomorrow for more free props!");
+      }
+
+      await bot.db.models.users.increment("props", { by: props, where: { id: rawData.uid } });
 
       return true;
     },
