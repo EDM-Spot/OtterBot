70100887a2bd2a14f743c3d263d1a7fd14cb9036
Marcelo Reis <marcelo-fil@outlook.com>
2021-01-10 03:43:08 +0000

    update

--- util/dashboard.js
+++ util/dashboard.js
@@ -67,6 +67,8 @@ module.exports = (client) => {
 
   app.use("/uno", express.static(path.resolve(`${__dirname}${path.sep}uno${path.sep}resources`)));
 
+  app.use("/public/images/badges", express.static(path.resolve(`${dataDir}${path.sep}public${path.sep}images`)));
+  app.use("/public/images/badges", express.static(path.resolve(`${dataDir}${path.sep}public${path.sep}images${path.sep}badges`)));
   app.use('/badges', express.static('public/images/badges'), serveIndex('public/images/badges', {'icons': true}));
 
   // These are... internal things related to passport. Honestly I have no clue either.
